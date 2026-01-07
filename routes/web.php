9a771cdbc73052797c56eafe339e80f9c90865b8
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-07 18:09:00 +0430

    routing and grouping and prefix

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,55 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    // return view('welcome');
-    // return "Welcome to Fanaven Educational Center\t Fanaven Technology";
-    // return 2+3;
-    return "Laravel v12 Specialist + Job by Fanaven Technology";
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     // return "Welcome to Fanaven Educational Center\t Fanaven Technology";
+//     // return 2+3;
+//     return "Laravel v12 Specialist + Job by Fanaven Technology";
+// });
 
-Route::get('message', function() {
-    return "Grow your coding with Fanaven Educaional Center";
+// Route::get('message', function() {
+//     return "Grow your coding with Fanaven Educaional Center";
+// });
+
+// Route::get('/customer/home', function() {
+//     return "Welcome to home page";
+// });
+
+// Route::get('/customer/user/{id}', function($id) {
+//     return "User ID ".$id." Welcome";
+// });
+
+// Route::get('/customer/user/{name}', function($name) {
+//     return "User ".$name." Welcome";
+// });
+
+// Route::get('/customer/order/{orderId}/product/{productId}', function($orderId,$productId) {
+//     return "Order $orderId with Product $productId is availble";
+// });
+
+// Route::get('/customer/category/{name?}', function($name='All') {
+//     return "Discount on $name category";
+// });
+
+Route::prefix('/customer/')->group(function() {
+    Route::get('/home', function() {
+        return "Welcome to home page";
+    });
+
+    Route::get('/user/{id}', function($id) {
+        return "User ID ".$id." Welcome";
+    });
+
+    Route::get('/user/{name}', function($name) {
+        return "User ".$name." Welcome";
+    });
+
+    Route::get('/order/{orderId}/product/{productId}', function($orderId,$productId) {
+        return "Order $orderId with Product $productId is availble";
+    });
+
+    Route::get('/category/{name?}', function($name='All') {
+        return "Discount on $name category";
+    });
 });
